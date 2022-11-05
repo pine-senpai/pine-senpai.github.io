4b6e80849b54bb899fd0645acaea5dc96d3759e8
pine-senpai <114869174+pine-senpai@users.noreply.github.com>
2022-11-05 17:24:07 +0300

    Update BluetoothTerminal.js

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -242,7 +242,7 @@ class BluetoothTerminal {
     this._log('Requesting bluetooth device...');
 
     return navigator.bluetooth.requestDevice({
-      filters: [{services: [this._serviceUuid]}],
+      filters: [],
     }).
         then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
