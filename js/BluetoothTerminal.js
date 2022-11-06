6ce2280f08111eee06d501023e19cb41c8871659
pine-senpai <114869174+pine-senpai@users.noreply.github.com>
2022-11-06 20:11:06 +0300

    Update BluetoothTerminal.js

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -9,7 +9,7 @@ class BluetoothTerminal {
    * @param {string} [receiveSeparator='\n'] - Receive separator
    * @param {string} [sendSeparator='\n'] - Send separator
    */
-  constructor(serviceUuid = 0x30, characteristicUuid = 0x31,
+  constructor(serviceUuid = 0xFFE0, characteristicUuid = 0xFFE1,
       receiveSeparator = '\n', sendSeparator = '\n') {
     // Used private variables.
     this._receiveBuffer = ''; // Buffer containing not separated data.
@@ -242,7 +242,7 @@ class BluetoothTerminal {
     this._log('Requesting bluetooth device...');
 
     return navigator.bluetooth.requestDevice({
-      filters: [{services: [this._serviceUuid]}],
+      filters: [{ имя: "Shagg" }],
     }).
         then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
